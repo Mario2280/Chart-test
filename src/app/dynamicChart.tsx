37800fd0a39a5ffc0977919b622ed1629b78ad49
David Gulkevich <david2017ra@gmail.com>
2023-08-15 13:12:54 +0300

    add y axis and change mouse event to pointer

--- src/app/dynamicChart.tsx
+++ src/app/dynamicChart.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { WheelEvent, useEffect, useState, MouseEvent, useRef } from 'react'
+import { WheelEvent, useEffect, useState, useRef, PointerEvent } from 'react'
 import * as PIXI from 'pixi.js'
 import Rectangle from './rectangle'
 import Head from 'next/head'
-import ZoomableTimeline from './timeline'
 
 interface Candle {
 	open: number
@@ -30,61 +29,101 @@ function generateRandomColumn(count: number) {
 	return data
 }
 
-const data = generateRandomColumn(30)
+const data = generateRandomColumn(100)
+
 const app = new PIXI.Application<HTMLCanvasElement>({
-	width: 3440,
-	height: 1440,
+	width: window.innerWidth,
+	height: window.innerHeight - 200,
 	backgroundColor: 0xffffff,
 })
+const xAxisCont = new PIXI.Container()
+const yAxisCont = new PIXI.Container()
+const chartCont = new PIXI.Container()
+
+
 app.stage.sortableChildren = true
 const MAX_ZOOM = 2
 const MIN_ZOOM = 0.5
 const STEP_ZOOM = 0.1
+
 export default function Chart() {
-	const CANVAS_WIDTH = 3440
-	const CANVAS_HEIGHT = 1440
+	const CANVAS_WIDTH = window.innerWidth 
+	const CANVAS_HEIGHT = window.innerHeight - 200;
 	const [zoomLevel, setZoomLevel] = useState(1)
 	const [panX, setPanX] = useState(0)
+	const [panY, setPanY] = useState(0)
+	const [isPointerDown, setIsPointerDown] = useState(false)
+
+	const [isSomeCandleMoved, setIsSomeCandleMoved] = useState(false)
 
 	const canvasRef = useRef<HTMLDivElement>(null)
 
 	useEffect(() => {
 		if (canvasRef.current) {
-			canvasRef.current.appendChild(app.view)
+			canvasRef.current.appendChild(app.view);
+			
 		}
 	}, [canvasRef])
 
+
 	useEffect(() => {
-		app.stage.removeChildren()
-		drawCandlestickChart()
-	}, [zoomLevel, panX])
+		console.log('1 time');
+		app.stage.addChild(xAxisCont)
+		app.stage.addChild(yAxisCont)
+		app.stage.addChild(chartCont);
+		drawCandlestickChart();
+		console.log(chartCont.position)
+		
+	}, []);
+	useEffect(() => {
+		console.log(chartCont.position)
+		chartCont.position.set(panX, -panY);
+		chartCont.scale.set(zoomLevel)
+		updateXAxis();
+		updateYAxis();
+	}, [zoomLevel, panX, panY])
 	return (
 		<>
 			<Head>
 				<title>Chart</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
-			<div ref={canvasRef} onMouseMove={handleMouseMove} onWheel={handleWheelMove}>
-				<ZoomableTimeline zoomLevel={zoomLevel} panX={panX} />
-			</div>
+
+			<div
+				ref={canvasRef}
+				onPointerMove={handlePointeMove}
+				onPointerDown={handlePointeDown}
+				onPointerUp={handlePointeUp}></div>
+			<button className='btn' onClick={handleZoomIn}>+</button>
+			<button className='btn' onClick={handleZoomOut}>-</button>
 		</>
 	)
 
-	function handleMouseMove(event: MouseEvent) {
-		if (event.altKey) return
-		if (event.buttons === 1) {
-			setPanX((prevPan) => (prevPan + event.movementX > 0 ? prevPan : prevPan + event.movementX))
+	function handlePointeMove(e: PointerEvent) {
+		if (isPointerDown) {
+			setPanX((prevPan) => prevPan + e.movementX)
+			setPanY((prevPan) => prevPan - e.movementY)
 		}
 	}
 
-	function handleWheelMove(event: WheelEvent) {
-		if (event.altKey) return
-		if (event.deltaY < 0) {
-			setZoomLevel((prevZoom) => Math.min(prevZoom + STEP_ZOOM, MAX_ZOOM))
-		} else {
-			setZoomLevel((prevZoom) => Math.max(prevZoom - STEP_ZOOM, MIN_ZOOM))
-		}
+	function handlePointeDown(e: PointerEvent) {
+		if (isSomeCandleMoved) return
+		setIsPointerDown(true)
 	}
+
+	function handlePointeUp(e: PointerEvent) {
+		setIsPointerDown(false)
+	}
+
+	function handleZoomIn(){
+		if (isSomeCandleMoved) return;
+		setZoomLevel((prevZoom) => Math.min(prevZoom + STEP_ZOOM, MAX_ZOOM))
+	}
+	function handleZoomOut() {
+		if (isSomeCandleMoved) return
+		setZoomLevel((prevZoom) => Math.max(prevZoom - STEP_ZOOM, MIN_ZOOM))
+	}
+	
 
 	function drawCandlestickChart() {
 		if (!app) return
@@ -99,15 +138,68 @@ export default function Chart() {
 		data.forEach((item, index) => {
 			const x = index * (candleWidth + candleSpacing) * zoomLevel + panX
 			const candleHeight = ((item.high - item.low) / valueRange) * CANVAS_HEIGHT * zoomLevel
-			const candleOffset = ((item.open - minValue) / valueRange) * CANVAS_HEIGHT * zoomLevel
-			new Rectangle({
+			const candleOffset = ((item.open - minValue) / valueRange) * CANVAS_HEIGHT * zoomLevel + panY
+			chartCont.addChild(new Rectangle({
 				x,
 				y: CANVAS_HEIGHT - candleOffset,
 				width: candleWidth * zoomLevel,
 				height: candleHeight,
 				color: item.close >= item.open ? 'green' : 'red',
 				app,
-			})
+				clickedFn: setIsSomeCandleMoved,
+			}))
 		})
 	}
+
+	function updateXAxis(){
+		const g = new PIXI.Graphics()
+		xAxisCont.children[0]?.destroy(true);
+		xAxisCont.removeChildren();
+		let xPos = 0;
+		const numericStep = 200
+		const HEIGHT = 50
+		let pos = -panX ;
+		while(xPos <= window.innerWidth){
+			let lineLength = pos % numericStep == 0 ? 15 : 2
+			g.lineStyle(2, 0xff0000).beginFill().moveTo(xPos, 0).lineTo(xPos, lineLength).endFill()
+
+			if (pos % numericStep === 0) {
+				
+				const text = new PIXI.Text(pos.toString());
+				g.addChild(text)
+				text.position.set(xPos, lineLength)
+			}
+
+			xPos += 1 * zoomLevel
+			pos += 1
+		}
+		xAxisCont.addChild(g)
+	}
+
+	function updateYAxis() {
+		yAxisCont.children[0]?.destroy(true);
+		yAxisCont.removeChildren()
+		const g = new PIXI.Graphics()
+
+		let yPos = 0
+		const numericStep = 200
+		const WIDTH = 50
+		let pos = panY
+		while (yPos <= window.innerHeight - 200) {
+			let lineLength = pos % numericStep == 0 ? 15 : 2
+			g.lineStyle(2, 0xff0000).beginFill().moveTo(0, yPos).lineTo(lineLength, yPos).endFill()
+
+			if (pos % numericStep === 0) {
+
+				const text = new PIXI.Text(pos.toString())
+				g.addChild(text)
+				text.position.set(lineLength, yPos)
+			}
+
+			yPos += 1 * zoomLevel
+			pos += 1
+		}
+		yAxisCont.addChild(g)
+	}
 }
+
--- src/app/rectangle.tsx
+++ src/app/rectangle.tsx
@@ -1,13 +1,15 @@
 import * as PIXI from 'pixi.js';
 import '@pixi/events';
 import { v4 as uuid } from 'uuid';
+import { Dispatch, SetStateAction } from 'react';
 export interface RectangleProps {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-  color: string;
-  app: PIXI.Application
+	x: number
+	y: number
+	width: number
+	height: number
+	color: string
+	app: PIXI.Application
+	clickedFn: Dispatch<SetStateAction<boolean>>
 }
 
 interface Draggable extends PIXI.DisplayObject {
@@ -23,35 +25,34 @@ export default class Rectangle extends PIXI.Graphics {
     this.drawRect(0, 0, props.width, props.height);
     this.endFill();
     this.eventMode = 'static';
-    this.on('mousedown', this.onDragStart);
-    this.on('mouseup', this.onDragEnd);
-    this.on('mouseupoutside', this.onDragEnd);
-    this.on('mousemove', this.onDragMove);
-    
-    const cont = new PIXI.Container();
-    cont.x = props.x;
-    cont.y = props.y
-    cont.addChild(this);
-    props.app.stage.addChild(cont);
+    this.on('pointerdown', this.onDragStart);
+    this.on('pointerup', this.onDragEnd);
+    this.on('pointerupoutside', this.onDragEnd);
+    this.on('pointermove', this.onDragMove);
+    this.position.set(props.x, props.y);
+  
+    props.app.stage.addChild(this)
   }
 
-  onDragStart = (event: PIXI.FederatedMouseEvent) => {
-  if(!event.altKey) return;
+onDragStart = (event: PIXI.FederatedPointerEvent) => {
+  
   const rect = event.currentTarget as Draggable;
   rect.data = event.data;
   rect.dragging = true;
   rect.alpha = 0.5;
   
   rect.parent.zIndex = 10
-  
+  this.props.clickedFn(true)
 }
-onDragEnd = (event: PIXI.FederatedMouseEvent) => {
+onDragEnd = (event: PIXI.FederatedPointerEvent) => {
   const rect = event.currentTarget as Draggable;
   rect.alpha = 1;
+  rect.parent.zIndex = 1;
   rect.dragging = false;
   rect.data = null;
+  this.props.clickedFn(false)
 };
-onDragMove = (event: PIXI.FederatedMouseEvent) => {
+onDragMove = (event: PIXI.FederatedPointerEvent) => {
   const rect = event.currentTarget as Draggable;
   if (rect.dragging) {
     const newPosition = rect.data!.getLocalPosition(rect.parent);
